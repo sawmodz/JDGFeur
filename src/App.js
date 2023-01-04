75d4af5827b6f6e391d17b7e8c39febdb8a1feb0
sawmodz <theowincke@live.fr>
2023-01-04 02:53:27 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import TextToSpeech from "./Component/TextToSpeech/TextToSpeech";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="main">
+        <h1>Quoi Feur Detector !</h1>
+        <TextToSpeech />
+        <h3>Site créer par BlizzFull; Discord : Blizz#7190</h3>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/Component/TextToSpeech/TextToSpeech.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import "./TextToSpeech.css";
+
+const wordList = require("../../wordList/word.json");
+
+const limitWord = 15;
+
+const TextToSpeech = () => {
+  const { transcript, resetTranscript } = useSpeechRecognition();
+  const [lastWord, setLastWord] = useState("");
+
+  useEffect(() => {
+    SpeechRecognition.startListening({ continuous: true });
+    console.log(transcript);
+    const myWord = transcript.split(" ");
+    if (wordList[myWord[myWord.length - 1]] != undefined) {
+      setLastWord(wordList[myWord[myWord.length - 1]]);
+    }
+    if (transcript.split(" ").length >= limitWord) {
+      resetTranscript();
+    }
+  });
+
+  return (
+    <div className="center">
+      <h2 className="text">{transcript}</h2>
+      <h1 className="reply">{lastWord}</h1>
+    </div>
+  );
+};
+
+export default TextToSpeech;
